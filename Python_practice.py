d2da398f7aed2414542c540af3372c4fa3e919ec
BrenyaSkaggs <93018678+BrenyaSkaggs@users.noreply.github.com>
2021-11-23 19:47:58 -0600

    Add files via upload

--- Python_practice.py
+++ Python_practice.py
@@ -1,22 +1,3 @@
-x=0
-while x <=5:
-    print (x)
-    x=x+1
-
-
-count = 7
-while count < 1:
-    print ("Hello World")
-
- 
-  
-numbers = [ 0,1,2,3,4]
-for num in numbers:
-    print(num)
-
-for num in range (5):
-    print(num)
-
 counties_dict= {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
 for county in counties_dict.keys():
     print(county)
@@ -29,12 +10,28 @@ for county in counties_dict:
 for county,voters in counties_dict.items():
     print (county,voters)
 for county,voters in counties_dict.items():
-    print(county + " county has"+ str("voters") + "registered voters")
+    print(county + " county has "+ str("voters") + " registered voters")
     
 
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+for county_dict in voting_data:
+    print(county_dict)              
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
+for county_dict in voting_data:
+  print(county_dict['county'])
 
+import os
 
-
+file_to_save= os.path.join("Resources","election_analysis.txt") 
+outfile=open(file_to_save, "w")
+outfile.write("")
+counties_dict = {"Arapahoe": 422829, "Denver":463353, "Jefferson": 432438}
+for county, voters in counties_dict.items():
+    print(county + " county has " + str(voters) + " registered voters.")
 
 
 
