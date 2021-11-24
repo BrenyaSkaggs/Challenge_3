9da03bbcb8e7c49fc07ea18a2e6ac1ed8ed98398
BrenyaSkaggs <93018678+BrenyaSkaggs@users.noreply.github.com>
2021-11-23 19:54:45 -0600

    Update Python_practice.py

--- Python_practice.py
+++ Python_practice.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 counties_dict= {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
 for county in counties_dict.keys():
     print(county)
@@ -35,3 +36,42 @@ for county, voters in counties_dict.items():
 
 
 
+=======
+counties_dict= {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county in counties_dict.keys():
+    print(county)
+for voters in counties_dict.values():
+    print(voters)
+for county in counties_dict:
+    print(counties_dict[county])
+for county in counties_dict:   
+    print(counties_dict.get(county))
+for county,voters in counties_dict.items():
+    print (county,voters)
+for county,voters in counties_dict.items():
+    print(county + " county has "+ str("voters") + " registered voters")
+    
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+for county_dict in voting_data:
+    print(county_dict)              
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
+for county_dict in voting_data:
+  print(county_dict['county'])
+
+import os
+
+file_to_save= os.path.join("Resources","election_analysis.txt") 
+outfile=open(file_to_save, "w")
+outfile.write("")
+counties_dict = {"Arapahoe": 422829, "Denver":463353, "Jefferson": 432438}
+for county, voters in counties_dict.items():
+    print(county + " county has " + str(voters) + " registered voters.")
+
+
+
+>>>>>>> 4b5ff6e915fc64441fc24e8f21cb902cbc17bfc4
